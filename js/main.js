43efed6811d44377f76910e99706a486f60415ab
Adie Williams <hello@adie.codes>
2017-04-20 14:28:19 +0100

    chore: Add TODO's and relevant ID's

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log("working");
+// TODO: Remove word as shouldn't be visible on page load
+// TODO: Store word as variable
+// TODO: Display underscore for each character in the word
+// TODO: When character guessed correctly reveal matches in word
+// TODO: When character guessed incorrectly add character to #guesses
+// TODO: When character guessed incorectly remove a life
+// TODO: When lives === 0, game over
+// TODO: When all characters of word guessed, game won
+// TODO: Use API to chose a category and then a random word
