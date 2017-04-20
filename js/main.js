6dfa7c4e77d6f1bb79a75527dcea13e709b4f619
Adie Williams <hello@adie.codes>
2017-04-20 14:38:07 +0100

    feat: Remove word and replace with underscores

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 console.log("working");
-// TODO: Remove word as shouldn't be visible on page load
-// TODO: Store word as variable
-// TODO: Display underscore for each character in the word
 // TODO: When character guessed correctly reveal matches in word
 // TODO: When character guessed incorrectly add character to #guesses
 // TODO: When character guessed incorectly remove a life
 // TODO: When lives === 0, game over
 // TODO: When all characters of word guessed, game won
 // TODO: Use API to chose a category and then a random word
+
+var hangman = (function() {
+	var word = 'Steve';
+	var hiddenWord = '';
+	var hiddenWordEle;
+	for (var i = 0; i < word.length; i++) {
+		hiddenWord += '_ ';
+	}
+	hiddenWordEle = document.createTextNode(hiddenWord);
+	document.getElementById('word').append(hiddenWordEle);
+})();
