bab26310a3647815e8c64ee2a5004ebf2c3f8986
Adie Williams <hello@adie.codes>
2017-04-21 11:35:10 +0100

    feat: Incorrect guess removes a life

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// TODO: added life count update but now need to refactor previous code to make better use of jQuery and simplify
 // TODO: When character guessed correctly reveal matches in word
 // TODO: When character guessed incorrectly add character to #guesses
 // TODO: When character guessed incorectly remove a life
@@ -7,34 +8,44 @@
 
 var hangman = (function() {
 	// on page load insert underscores for each character in the word
+	// setting variables to be accessible thoughout program
+	var gameWord;
+	// turn game word to array to make easier to replace
+	var gameWordToArr;
+	// Set number of lives
+	var lifeCount = 10;
+	var lifeCountEle = document.createElement("span");
+	lifeCountEle.id = 'gameLives';
+	lifeCountEle.append(lifeCount);
+	// setting variable to act as HTML node to add underscores to page
+	var hiddenWordEle;
 	var insertWord = (function() {
 		// by chosing a word
-		var word = 'Steve';
+		gameWord = 'Steve';
+		gameWord = gameWord.toLowerCase();
 		// setting variable to store underscores
 		var hiddenWord = '';
-		// setting variable to act as HTML node to add underscores to page
-		var hiddenWordEle;
 		// loop each character of word and add underscore
-		for (var i = 0; i < word.length; i++) {
+		for (var i = 0; i < gameWord.length; i++) {
 			hiddenWord += '_ ';
 		}
-		// create text node
+		// create text node for game word and life count
 		hiddenWordEle = document.createTextNode(hiddenWord);
-		// add text node to page
+		// add text node to page for game word
 		document.getElementById('word').append(hiddenWordEle);
+		// add text node to page for lives
+		document.getElementById('lives').append(lifeCountEle);
 	})();
 	// store previous guesses outside of function to stop it getting reset
 	var previousGuesses = [];
 	document.getElementById('guessSubmit').addEventListener('click', function() {
 		// set current guess to input
-		var currentGuess = document.getElementById('guess').value;
+		var currentGuess = document.getElementById('guess').value.toLowerCase();
 		// add current guess to previous guess array
-		if (previousGuesses.length === 0) {
+		if (previousGuesses.length === 0 || previousGuesses.indexOf(currentGuess) === -1) {
 			previousGuesses[0] = currentGuess;
 			document.getElementById('previousGuesses').append(currentGuess + ' ');
-		} else if (previousGuesses.indexOf(currentGuess) === -1) {
-			previousGuesses.push(currentGuess);
-			document.getElementById('previousGuesses').append(currentGuess + ' ');
+			checkGuess(currentGuess);
 		} else {
 			// if letter already guessed return alert
 			alert("Already guessed this letter");
@@ -42,4 +53,15 @@ var hangman = (function() {
 		// display guess on page
 		document.getElementById('guess').value = '';
 	});
+	var lifeEle = document.getElementById('lives');
+	var checkGuess = function(char) {
+		gameWordToArr = gameWord.split('');
+		console.log(gameWordToArr);
+		if (gameWordToArr.indexOf(char) === -1) {
+			lifeCount--;
+			//lifeEle.removeChild(lifeCountEle);
+			var span = '<span id="gameLives">' + lifeCount + '</span>';
+			$('#gameLives').replaceWith(span);
+		}
+	};
 })();
